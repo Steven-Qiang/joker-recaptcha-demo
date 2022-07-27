26562779a15543dc6ef9c5d94ce3f8e59869aa69
qiangmouren <2962051004@qq.com>
2022-07-27 12:47:43 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -5,17 +5,26 @@
  * @create: 2021-09-11 04:25:20
  * @author: qiangmouren (2962051004@qq.com)
  * -----
- * @last-modified: 2022-07-26 02:16:45
+ * @last-modified: 2022-07-27 12:47:08
  * -----
  */
 module.exports = {
-    productionSourceMap: false,
-    publicPath: process.env.NODE_ENV === 'production' ? '/joke-recaptcha-demo/' : '',
-    configureWebpack: config => {
-
-        config.externals = {
-            'vue': 'Vue',
-            'ant-design-vue': 'antd'
-        };
-    },
-}
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/joke-recaptcha-demo/' : '',
+  chainWebpack: (config) => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .loader('url-loader')
+      .tap((options) => {
+        options.limit = Infinity;
+        return options;
+      });
+  },
+  configureWebpack: (config) => {
+    config.externals = {
+      vue: 'Vue',
+      'ant-design-vue': 'antd',
+    };
+  },
+};
