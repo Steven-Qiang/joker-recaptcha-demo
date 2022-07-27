d96f85fb1f443f93614333f34d80285bf75b0ad5
qiangmouren <2962051004@qq.com>
2022-07-27 13:56:32 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -5,22 +5,12 @@
  * @create: 2021-09-11 04:25:20
  * @author: qiangmouren (2962051004@qq.com)
  * -----
- * @last-modified: 2022-07-27 12:47:08
+ * @last-modified: 2022-07-27 01:55:34
  * -----
  */
 module.exports = {
   productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'production' ? '/joke-recaptcha-demo/' : '',
-  chainWebpack: (config) => {
-    config.module
-      .rule('images')
-      .use('url-loader')
-      .loader('url-loader')
-      .tap((options) => {
-        options.limit = Infinity;
-        return options;
-      });
-  },
   configureWebpack: (config) => {
     config.externals = {
       vue: 'Vue',
